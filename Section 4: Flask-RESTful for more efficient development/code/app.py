eecb5d98f3d6ac24af3a8c9856bc88bc94df0d6f
Ben Chavez <ben.m.chavez@gmail.com>
2020-07-23 16:32:26 -0500

    creating item resource

--- Section 4: Flask-RESTful for more efficient development/code/app.py
+++ Section 4: Flask-RESTful for more efficient development/code/app.py
@@ -4,14 +4,24 @@ from flask_restful import Resource, Api
 app = Flask(__name__)
 api = Api(app)
 
+items = []
 
-# API works with resources and every resource must be a class (Student is a resource that can
+
+# API works with resources and every resource must be a class (Item is a resource that can
 # only be accessed with a GET method)
-class Student(Resource):
+class Item(Resource):
     def get(self, name):
-        return {'student': name}
+        for item in items:
+            if item['name'] == name:
+                return item
+        return {'item': None}, 404
+
+    def post(self, name):
+        item = {'name': name, 'price': 12.00}
+        items.append(item)
+        return item,
 
 
-api.add_resource(Student, '/student/<string:name>')     # adding route/endpoint
+api.add_resource(Item, '/item/<string:name>')     # adding route/endpoint
 
 app.run(port=5000)
