dff33b0c1374db4e8bcc5bbbaf8144030b4287ad
Ben Chavez <ben.m.chavez@gmail.com>
2020-07-23 16:45:53 -0500

    the itemList and creating items

--- Section 4: Flask-RESTful for more efficient development/code/app.py
+++ Section 4: Flask-RESTful for more efficient development/code/app.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, request
 from flask_restful import Resource, Api
 
 app = Flask(__name__)
@@ -17,11 +17,20 @@ class Item(Resource):
         return {'item': None}, 404
 
     def post(self, name):
-        item = {'name': name, 'price': 12.00}
+        # data = request.get_json(force=True)   - force=True will format content-Type header even if it is not set to application/json
+        # data = request.get_json(silent=True)  - silent=True will not send the 'no-header' error
+        data = request.get_json()
+        item = {'name': name, 'price': data['price']}
         items.append(item)
         return item,
 
 
-api.add_resource(Item, '/item/<string:name>')     # adding route/endpoint
+class ItemList(Resource):
+    def get(self):
+        return {'items': items}
 
-app.run(port=5000)
+
+api.add_resource(Item, '/item/<string:name>')     # adding route/endpoint
+api.add_resource(ItemList, '/items')
+
+app.run(port=5000, debug=True)
