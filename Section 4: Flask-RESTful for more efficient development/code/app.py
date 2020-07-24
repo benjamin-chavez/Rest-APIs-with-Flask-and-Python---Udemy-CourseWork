5f1a01ca3be7870a7cc4c34b8511929fa1c5392a
Ben Chavez <ben.m.chavez@gmail.com>
2020-07-23 19:30:20 -0500

    authenitcation and logging in pt2

--- Section 4: Flask-RESTful for more efficient development/code/app.py
+++ Section 4: Flask-RESTful for more efficient development/code/app.py
@@ -1,9 +1,14 @@
 from flask import Flask, request
 from flask_restful import Resource, Api
+from flask_jwt import JWT, jwt_required
+
+from security import authenticate, identity
 
 app = Flask(__name__)
 app.secret_key = 'Benny'
 api = Api(app)
+
+jwt = JWT(app, authenticate, identity)  # new endpoint: "/auth"
 
 items = []
 
@@ -11,6 +16,7 @@ items = []
 # API works with resources and every resource must be a class (Item is a resource that can
 # only be accessed with a GET method)
 class Item(Resource):
+    @jwt_required()
     def get(self, name):
         # for item in items:
         #     if item['name'] == name:
