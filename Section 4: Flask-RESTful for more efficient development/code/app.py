651bb6723c4b50caf42c956830f33396acc0f344
Ben Chavez <ben.m.chavez@gmail.com>
2020-07-27 17:06:14 -0500

    optimizing final code/setting parser class variable

--- Section 4: Flask-RESTful for more efficient development/code/app.py
+++ Section 4: Flask-RESTful for more efficient development/code/app.py
@@ -1,5 +1,5 @@
 from flask import Flask, request
-from flask_restful import Resource, Api
+from flask_restful import Resource, Api, reqparse
 from flask_jwt import JWT, jwt_required
 
 from security import authenticate, identity
@@ -16,6 +16,13 @@ items = []
 # API works with resources and every resource must be a class (Item is a resource that can
 # only be accessed with a GET method)
 class Item(Resource):
+    parser = reqparse.RequestParser()
+    parser.add_argument('price',
+                        type=float,
+                        required=True,
+                        help="This field cannot be left blank!"
+                        )
+
     @jwt_required()
     def get(self, name):
         # for item in items:
@@ -32,20 +39,24 @@ class Item(Resource):
             return {'message': "An item with name '{}' already exists.".format(name)}, 400
         # data = request.get_json(force=True)   - force=True will format content-Type header even if it is not set to application/json
         # data = request.get_json(silent=True)  - silent=True will not send the 'no-header' error
-        data = request.get_json()
+        # data = request.get_json()
+        data = Item.parser.parse_args()
+
         item = {'name': name, 'price': data['price']}
         items.append(item)
         return item,
 
-    @jwt_required()
+    # @jwt_required()
     def delete(self, name):
         global items
         items = list(filter(lambda x: x['name'] != name, items))
         return {'message': 'Item deleted'}
 
-    @jwt_required()
+    # @jwt_required()
     def put(self, name):
-        data = request.get_json()
+        # data = request.get_json()
+        data = Item.parser.parse_args()
+
         item = next(filter(lambda x: x['name'] == name, items), None)
         if item is None:
             item = {'name': name, 'price': data['price']}
