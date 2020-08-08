183ad0082145b9935836c7383227be1f8f0d8d55
Ben Chavez <ben.m.chavez@gmail.com>
2020-08-07 20:11:19 -0500

    sections 7 and 8

--- Section 8: Deploying Flask apps to Heroku/code/app.py
+++ Section 8: Deploying Flask apps to Heroku/code/app.py
@@ -1,7 +1,9 @@
+import os
+
 from flask import Flask
 from flask_restful import Api
 from flask_jwt import JWT
-from db import db
+
 
 from security import authenticate, identity
 from resources.user import UserRegister
@@ -9,20 +11,14 @@ from resources.item import Item, ItemList
 from resources.store import Store, StoreList
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///data.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get(
+    'DATABASE_URL', 'sqlite:///data.db')
 # Specify SQLAlchemy configuration property - turns off Flask SQLAlchemy tracker, and instead uses the SQLAlchemy main library tracker
 app.config['SQLALCHEMY_TRACK_MODIFACTIONS'] = False
 app.secret_key = 'Benny'
 api = Api(app)
 
-# The following decorator will create all the tables unless they exist already
-@app.before_first_request
-def create_tables():
-    db.create_all()
-
-
 jwt = JWT(app, authenticate, identity)  # new endpoint: "/auth"
-
 
 api.add_resource(Store, '/store/<string:name>')
 api.add_resource(Item, '/item/<string:name>')     # adding route/endpoint
@@ -35,5 +31,6 @@ api.add_resource(UserRegister, '/register')
 # in the case where we were importing something from the app.py
 # file. Instead it will only run the app when we run this file.
 if __name__ == '__main__':
+    from db import db
     db.init_app(app)
     app.run(port=5000, debug=True)
---
+++ Section 8: Deploying Flask apps to Heroku/code/run.py
@@ -0,0 +1,9 @@
+from app import app
+from db import db
+
+db.init_app(app)
+
+# The following decorator will create all the tables unless they exist already
+@app.before_first_request
+def create_tables():
+    db.create_all()
