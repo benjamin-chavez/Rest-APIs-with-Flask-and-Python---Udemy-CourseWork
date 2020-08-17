fabc17f764e30640562cbafcb0e7336ba9971658
Ben Chavez <ben.m.chavez@gmail.com>
2020-08-16 21:41:32 -0500

    return different amounts of information depending on whether a user is logged in or not. used jwt_optional, and get_jwt_identity

--- Section 11: Token refreshing and Flask-JWT-Extended/code/resources/item.py
+++ Section 11: Token refreshing and Flask-JWT-Extended/code/resources/item.py
@@ -1,5 +1,5 @@
 from flask_restful import Resource, reqparse
-from flask_jwt_extended import jwt_required, get_jwt_claims
+from flask_jwt_extended import jwt_required, get_jwt_claims, jwt_optional, get_jwt_identity
 from models.item import ItemModel
 
 
@@ -66,5 +66,13 @@ class Item(Resource):
 
 
 class ItemList(Resource):
+    @jwt_optional
     def get(self):
-        return {'items': [x.json() for x in ItemModel.find_all()]}
+        user_id = get_jwt_identity()
+        items = [item.json() for item in ItemModel.find_all()]
+        if user_id:
+            return {'items': items}, 200
+        return {
+            'items': [item['name'] for item in items],
+            'message': 'More data available if you login.'
+        }, 200
