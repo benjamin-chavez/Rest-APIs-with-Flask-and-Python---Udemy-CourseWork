2218b983676b1f05ece5f58b5f19f0b191d27edc
Ben Chavez <ben.m.chavez@gmail.com>
2020-08-17 15:24:18 -0500

    added user id blacklist to api

--- Section 11: Token refreshing and Flask-JWT-Extended/code/app.py
+++ Section 11: Token refreshing and Flask-JWT-Extended/code/app.py
@@ -1,16 +1,22 @@
-from flask import Flask
+from flask import Flask, jsonify
 from flask_restful import Api
 from flask_jwt_extended import JWTManager
 
 from resources.user import UserRegister, User, UserLogin, TokenRefresh
 from resources.item import Item, ItemList
 from resources.store import Store, StoreList
+from blacklist import BLACKLIST
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///data.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 # Allows exceptions to return their own errors
 app.config['PROPAGATE_EXCEPTIONS'] = True
+app.config['JWT_BLACKLIST_ENABLED'] = True  # enable blacklist feature
+# allow blacklisting for access and refresh tokens
+app.config['JWT_BLACKLIST_TOKEN_CHECKS'] = ['access', 'refresh']
+
+
 app.secret_key = 'jose'  # app.config['JWT_SECRET_KEY']
 api = Api(app)
 
@@ -31,6 +37,11 @@ def add_claims_to_jwt(identity):
     return {'is_admin': False}
 
 
+@jwt.token_in_blacklist_loader
+def check_if_token_in_blacklist(decrypted_token):
+    return decrypted_token['identity'] in BLACKLIST
+
+
 @jwt.expired_token_loader
 def expired_token_callback():
     return jsonify({
@@ -42,7 +53,7 @@ def expired_token_callback():
 @jwt.invalid_token_loader
 def invalid_token_callback(error):
     return jsonify({
-        'description': 'Signature verifation failed',
+        'message': 'Signature verification failed.',
         'error': 'invalid_token'
     }), 401
 
